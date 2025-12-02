040dceda5d3b14c237f227c292852e8b060c8a7b
Amisha Singh <amishasingh@example.com>
2025-12-02 19:07:45 +0530

    vehicle

---
+++ src/Vehicle.java
@@ -0,0 +1,25 @@
+
+class Vehicle {
+    public void displayInfo() {
+        this.brand = brand;
+        this.fuel = fuel;
+        System.out.println(brand);
+        System.out.println(fuel);
+        //show
+    }
+
+    public int fuelUp() {
+        //adds 10 liters
+
+
+    }
+
+    public int fuelUp() {
+        //add given number of liters
+
+    }
+
+    public void travelDistance() {
+        //double efficiency
+    }
+}
